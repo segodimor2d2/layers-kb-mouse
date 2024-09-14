fbe096a174d596b7ebddb52003c5c7a824037abb
segodimo <segodimo@gmail.com>
2024-09-14 13:23:39 -0300

    00mappings.md

---
+++ 00layer.sh
@@ -0,0 +1,9 @@
+#xmodmap -e "keycode 32 = a"
+#xmodmap -e "keycode 32 = NoSymbol"
+
+# keycode  64 = Alt_L Meta_L Alt_L Meta_L
+xmodmap -e "keycode 64 = Super_L"
+
+# keycode 133 = Super_L NoSymbol Super_L
+xmodmap -e "keycode 133 = Alt_L"
+
--- 66ikb.py
+++ 66ikb.py
@@ -14,37 +14,21 @@ def on_press(key):
         pressed_keys.add(keycode)
         print(pressed_keys)
     except AttributeError:
-        # Para teclas especiais, você pode adicionar tratamento adicional aqui
         pass
 
     if 65515 in pressed_keys and 16777215 in pressed_keys:  # win + caps
         print('layers')
 
+    # Para sair do programa use win + esc
     if 65515 in pressed_keys and 65307 in pressed_keys:  # win + esc
         return False
-    #65307
-
-    # if keyboard.KeyCode.from_vk(16777215) in pressed_keys and keyboard.KeyCode.from_vk(65515) in pressed_keys:
-    #     print(888888)
-
-    # if keyboard.KeyCode.from_char('a') in pressed_keys and keyboard.KeyCode.from_char('d') in pressed_keys:
-    #     print("As teclas 'a' e 'd' estão sendo pressionadas simultaneamente")
-    # else:
-    #     # Imprime a tecla que está sendo pressionada
-    #     # print(f"Tecla pressionada: {key}")
-    #     pass
 
 def on_release(key):
     try:
-        # Remove a tecla liberada do conjunto
         keycode = key.vk if hasattr(key, 'vk') else key.value.vk
         pressed_keys.discard(keycode)
     except AttributeError:
         pass
-
-    # Para sair do programa quando a tecla ESC é pressionada
-    # if key == keyboard.Key.esc:
-    #     return False
 
 # Configura o Listener
 with keyboard.Listener(
