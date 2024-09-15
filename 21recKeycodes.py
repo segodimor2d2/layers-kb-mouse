4c84b4c023d070cd7ea776c55f4e9e1dad913ea8
segodimo <segodimo@gmail.com>
2024-09-15 06:19:27 -0300

    21 rodando win e esc modo mouse

--- 21recKeycodes.py
+++ 21recKeycodes.py
@@ -9,8 +9,6 @@ import subprocess
 
 mouse_controller = mouse.Controller()
 
-#subprocess.run(['./91layer0.sh'], shell=True, check=True)
-subprocess.run(['./93layer2.sh'], shell=True, check=True)
 
 def move_mouse(x, y):
     mouse_controller.move(x, y)
@@ -25,51 +23,55 @@ def on_press(key):
     try:
         # Adiciona a tecla pressionada ao conjunto
         keycode = key.vk if hasattr(key, 'vk') else key.value.vk
-        print(key)
+        print(keycode)
 
         pressed_keys.add(keycode)
 
     except AttributeError:
         pass
 
-    if 65515 in pressed_keys and 65515 in pressed_keys:  # win + caps
-        subprocess.run(['./91layer0.sh'], shell=True, check=True)
+    # Para sair do programa use win + esc
+    # if 65515 in pressed_keys and 65307 in pressed_keys:  # win + esc
+    if 65307 in pressed_keys:  # esc
+        # subprocess.run(['./91layer0.sh'], shell=True, check=True)
+        subprocess.run(['./93layer2.sh'], shell=True, check=True)
         print('layers 88888888')
 
-    # Para sair do programa use win + esc
-    if 65515 in pressed_keys and 65307 in pressed_keys:  # win + esc
-        # subprocess.run(['setxkbmap'], shell=True, check=True)
+        if keycode == 107: # k
+            move_mouse(0, -10)
+
+        elif keycode == 106: # j
+            move_mouse(0, 10)
+
+        elif keycode == 104: # h
+            move_mouse(-10, 0)
+
+        elif keycode == 108: # l
+            move_mouse(10, 0)
+
+        elif keycode == 102: # f
+            click_mouse(mouse.Button.left)
+
+        elif keycode == 100: # d
+            click_mouse(mouse.Button.middle)
+
+        elif keycode == 115: # s
+            click_mouse(mouse.Button.right)
+
+
+    # if 65515 in pressed_keys and 65515 in pressed_keys:  # win + caps
+    if 65515 in pressed_keys: # win
+        subprocess.run(['setxkbmap'], shell=True, check=True)
         return False
-
-    if keycode == 107: # k
-        move_mouse(0, -10)
-
-    elif keycode == 106: # j
-        move_mouse(0, 10)
-
-    elif keycode == 104: # h
-        move_mouse(-10, 0)
-
-    elif keycode == 108: # l
-        move_mouse(10, 0)
-
-    elif keycode == 102: # f
-        click_mouse(mouse.Button.left)
-
-    elif keycode == 100: # d
-        click_mouse(mouse.Button.middle)
-
-    elif keycode == 115: # s
-        click_mouse(mouse.Button.right)
 
 def on_release(key):
     try:
         keycode = key.vk if hasattr(key, 'vk') else key.value.vk
         pressed_keys.discard(keycode)
 
-        if key == keyboard.Key.esc:
-            subprocess.run(['setxkbmap'], shell=True, check=True)
-            return False  # Interrompe o listener
+        # if key == keyboard.Key.esc:
+        #     subprocess.run(['setxkbmap'], shell=True, check=True)
+        #     return False  # Interrompe o listener
 
     except AttributeError:
         pass
--- 22tst.py
+++ 22tst.py
@@ -7,8 +7,6 @@ layerNum = 0
 # Inicializar o controlador de mouse
 mouse_controller = mouse.Controller()
 
-subprocess.run(['./93layer2.sh'], shell=True, check=True)
-
 # Funções para mover o mouse
 def move_mouse(x, y):
     mouse_controller.move(x, y)
@@ -16,7 +14,13 @@ def move_mouse(x, y):
 def click_mouse(button):
     mouse_controller.click(button)
 
+def change_layout(layout_file):
+    #subprocess.run([layout_file], check=True)
+    subprocess.run([layout_file], shell=True, check=True)
+
 def on_press(key):
+    global leader01
+    global layerNum
 
     try:
         # Obtenha o código da tecla pressionada
@@ -26,45 +30,51 @@ def on_press(key):
         # print(f"Tecla pressionada: {key}")
         keycode = None
 
-    if keycode == 107: # k
+    if keycode == 65515:
+        layerNum = leader01 % 3
+
+        if layerNum == 0:
+            print("layer normal")
+            change_layout('setxkbmap')
+
+        elif layerNum == 1:
+            print("layer primeiro")
+            change_layout('./92layer1.sh')
+        elif layerNum == 2:
+            print("layer segundo")
+            change_layout('./93layer2.sh')
+
+        leader01 += 1
+
+    if keycode == 107 and layerNum == 2: # k
         move_mouse(0, -10)
 
-    elif keycode == 106: # j
+    elif keycode == 106 and layerNum == 2: # j
         move_mouse(0, 10)
 
-    elif keycode == 104: # h
+    elif keycode == 104 and layerNum == 2: # h
         move_mouse(-10, 0)
 
-    elif keycode == 108: # l
+    elif keycode == 108 and layerNum == 2: # l
         move_mouse(10, 0)
 
-    elif keycode == 102: # f
+    elif keycode == 102 and layerNum == 2: # f
         click_mouse(mouse.Button.left)
 
-    elif keycode == 100: # d
+    elif keycode == 100 and layerNum == 2: # d
         click_mouse(mouse.Button.middle)
 
-    elif keycode == 115: # s
+    elif keycode == 115 and layerNum == 2: # s
         click_mouse(mouse.Button.right)
 
     return True  # Continua a escutar e não interrompe
 
 def on_release(key):
-    try:
-        keycode = key.vk if hasattr(key, 'vk') else key.value.vk
+    # Saia do programa quando a tecla 'esc' for pressionada
+    if key == keyboard.Key.esc:
+        return False
 
-        if key == keyboard.Key.esc:
-            subprocess.run(['setxkbmap'], shell=True, check=True)
-            return False  # Interrompe o listener
-
-    except AttributeError:
-        pass
-
-# Configura o Listener
-with keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release
-) as listener:
+# Configurar o listener para o teclado
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
 
